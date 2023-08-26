bb9cf732e7cc91e1606574d26876998f088f8795
Chris Zimmermann <chris.zimmermann@hotmail.ch>
2023-08-26 17:05:19 +0200

    Initial Commit

---
+++ todooz.mk1/todooz_mk1App.swift
@@ -0,0 +1,17 @@
+//
+//  todooz_mk1App.swift
+//  todooz.mk1
+//
+//  Created by Chris Zimmermann on 26.08.23.
+//
+
+import SwiftUI
+
+@main
+struct todooz_mk1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
